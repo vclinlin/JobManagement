e24df57c2f72f694b4d0a56d159a41fe378307bd
知否 <vclinlin@vip.qq.com>
2018-09-17 17:25:47 +0800

    修复

--- public/static/js/index/teachers/main.js
+++ public/static/js/index/teachers/main.js
@@ -1,11 +1,11 @@
 $(function (){
-    $("#main_nav").addClass('fixed-top');
-    // console.log($("#main_nav")["0"].clientHeight); //打印一下当前导航栏高度
-    $(".line-one").css('margin-top',$("#main_nav")[0].clientHeight+'px'); //偏移下方内容所在位置 <公共属性 行一>
-
     //美化所有分页
     $(".pagination").find('span,a').attr('class','page-link');
     $(".pagination").find('li').attr('class','page-item');
+
+    $("#main_nav").addClass('fixed-top');
+    // console.log($("#main_nav")["0"].clientHeight); //打印一下当前导航栏高度
+    $(".line-one").css('margin-top',$("#main_nav")[0].clientHeight+'px'); //偏移下方内容所在位置 <公共属性 行一>
 });
 function OpenUrl(_this) {
     window.location.href=_this.dataset.url;
